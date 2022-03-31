3911313b6142fd1131f264e7857e9b1e68b17b5f
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-03-31 09:50:40 +0100

    first commit

---
+++ TACShilohDistrict/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TACShilohDistrict.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
